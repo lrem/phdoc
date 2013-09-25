57e7d22d6464787d094ab76dc8ae6982e917b849
Remigiusz Modrzejewski <lrem@maxnet.org.pl>
2013-09-25 18:02:49 +0200

    Testing the Markdown extensions introduced over Markdoc.

--- src/phdoc/mdx_mathjax.py
+++ src/phdoc/mdx_mathjax.py
@@ -4,20 +4,38 @@ This near trivial extension makes MathJax formulas AtomicStrings.
 
 import markdown
 
+
 class MathJaxInlinePattern(markdown.inlinepatterns.Pattern):
     """
-    Pattern for in-line math.
+    Pattern for in-line math, recommended.
     """
 
     def __init__(self):
-        markdown.inlinepatterns.Pattern.__init__(self, 
+        markdown.inlinepatterns.Pattern.__init__(self,
                 r'(?<!\\)(\\\()(.+?)(\\\))')
 
     def handleMatch(self, m):
         # This would be better if I could return AtomicString...
         node = markdown.util.etree.Element('span')
-        node.text = markdown.util.AtomicString('\('+ m.group(3) + '\)')
+        node.text = markdown.util.AtomicString('\(' + m.group(3) + '\)')
         return node
+
+
+class MathJaxInlinePattern2(markdown.inlinepatterns.Pattern):
+    """
+    Pattern for in-line math, single-dollars.
+    """
+
+    def __init__(self):
+        markdown.inlinepatterns.Pattern.__init__(self,
+                r'(?<!\\)(\$)(.+?)(\$)')
+
+    def handleMatch(self, m):
+        # This would be better if I could return AtomicString...
+        node = markdown.util.etree.Element('span')
+        node.text = markdown.util.AtomicString('$' + m.group(3) + '$')
+        return node
+
 
 class MathJaxBlockPattern(markdown.inlinepatterns.Pattern):
     """
@@ -26,21 +44,24 @@ class MathJaxBlockPattern(markdown.inlinepatterns.Pattern):
     """
 
     def __init__(self):
-        markdown.inlinepatterns.Pattern.__init__(self, 
+        markdown.inlinepatterns.Pattern.__init__(self,
                 r'(?<!\\)(\\\[)(.+?)(\\\])')
 
     def handleMatch(self, m):
         # This would be better if I could return AtomicString...
-        node = markdown.util.etree.Element('mathjaxb')
-        node.text = markdown.util.AtomicString('\['+ m.group(3) + '\]')
+        node = markdown.util.etree.Element('span')
+        node.text = markdown.util.AtomicString('\[' + m.group(3) + '\]')
         return node
+
 
 class MathJaxExtension(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
-        # Needs to come before escape matching because \ is pretty important 
+        # Needs to come before escape matching because \ is pretty important
         # in LaTeX
         md.inlinePatterns.add('mathjaxi', MathJaxInlinePattern(), '<escape')
+        md.inlinePatterns.add('mathjaxi2', MathJaxInlinePattern2(), '<escape')
         md.inlinePatterns.add('mathjaxb', MathJaxBlockPattern(), '<escape')
+
 
 def makeExtension(configs=None):
     return MathJaxExtension(configs)
---
+++ test/example/wiki/file6.md
@@ -0,0 +1,3 @@
+Abstract:
+
+: hidden
